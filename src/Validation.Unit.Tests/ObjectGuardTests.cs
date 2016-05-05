04a51468808274162ab4e1d36622780fcbefbbd1
Greg McGuffey <greg.mcguffey@cognosante.com>
2016-05-04 17:14:46 -0700

    Added object guards and tests.

---
+++ src/Validation.Unit.Tests/ObjectGuardTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Gpm.Validation;
+
+namespace Gpm.Validation.Unit.Tests
+{
+    [TestFixture]
+    public class ObjectGuardTests
+    {
+        [Test]
+        public void GuardIsNotNull_WithNull_ThrowsException()
+        {
+            //-- any reference type will work.
+            string param = null;
+            var paramName = "param";
+            var exceptionMessage = "You must provide a parameter.";
+
+            var ex = Assert.Throws<ArgumentNullException>(() => param.GuardIsNotNull(paramName, exceptionMessage));
+
+            StringAssert.Contains(exceptionMessage, ex.Message);
+        }
+
+        [Test]
+        public void GuardIsNotNull_WithItem_ReturnsItem()
+        {
+            var param = "hello World";
+            var paramName = "param";
+            var exceptionMessage = "You must provide a parameter.";
+
+            var returned = param.GuardIsNotNull(paramName, exceptionMessage);
+
+            Assert.AreSame(param, returned);
+        }
+
+        [Test]
+        public void GuardIsNotNull_WithValueType_ReturnsItem()
+        {
+            // Test if an item that can't be null, works.
+            int param = 1;
+            var paramName = "param";
+            var exceptionMessage = "Should never see this if value can't be null.";
+
+            var returned = param.GuardIsNotNull(paramName, exceptionMessage);
+
+            Assert.AreEqual(param, returned);
+        }
+
+        [Test]
+        public void GuardIsRequiredForOperation_WithNull_ThrowsException()
+        {
+            //-- any reference type will work.
+            string param = null;
+            var exceptionMessage = "The following operation can't be completed.";
+
+            var ex = Assert.Throws<InvalidOperationException>(() => param.GuardIsRequiredForOperation(exceptionMessage));
+
+            StringAssert.Contains(exceptionMessage, ex.Message);
+        }
+
+        [Test]
+        public void GuardIsRequiredForOperation_WithItem_ReturnsItem()
+        {
+            var param = "hello World";
+            var exceptionMessage = "The following operation can't be completed.";
+
+            var returned = param.GuardIsRequiredForOperation(exceptionMessage);
+
+            Assert.AreSame(param, returned);
+        }
+
+        [Test]
+        public void GuardIsRequiredForOperation_WithValueType_ReturnsItem()
+        {
+            // Test if an item that can't be null, works.
+            int param = 1;
+            var exceptionMessage = "Should never see this if value can't be null.";
+
+            var returned = param.GuardIsRequiredForOperation(exceptionMessage);
+
+            Assert.AreEqual(param, returned);
+        }
+
+        [Test]
+        public void GuardIsNotDefault_WithNullObject_ThrowsException()
+        {
+            // Arrange
+            var nullMessage = "The item cannot be null.";
+            string nullItem = null;
+            var paramName = "param";
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => nullItem.GuardIsNotDefault<string>(paramName));
+
+            // Assert
+            StringAssert.Contains(expected: nullMessage, actual: ex.Message);
+        }
+
+        [Test]
+        public void GuardIsNotDefault_WithDefaultInt_ThrowsException()
+        {
+            // Arrange
+            var defaultMessage = "The item cannot be the default value for this type (Int32).";
+            int defaultItem = default(int);
+            var paramName = "param";
+
+            // Act
+            var ex = Assert.Throws<ArgumentException>(() => defaultItem.GuardIsNotDefault<int>(paramName));
+
+            // Assert
+            StringAssert.Contains(expected: defaultMessage, actual: ex.Message);
+        }
+
+        [Test]
+        public void GuardIsNotDefault_WithNonNull_ReturnsItem()
+        {
+            // Arrange
+            var nonNullItem = "a";
+            var paramName = "param";
+
+            // Act
+            var returnedItem = nonNullItem.GuardIsNotDefault<string>(paramName);
+
+            // Assert
+            Assert.AreEqual(expected: nonNullItem, actual: returnedItem);
+        }
+
+        [Test]
+        public void GuardIsNotDefault_WithNonDefault_ReturnsItem()
+        {
+            // Arrange
+            var nonNullItem = 1;
+            var paramName = "param";
+
+            // Act
+            var returnedItem = nonNullItem.GuardIsNotDefault<int>(paramName);
+
+            // Assert
+            Assert.AreEqual(expected: nonNullItem, actual: returnedItem);
+        }
+    }
+}
---
+++ src/Validation/ObjectGuards.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gpm.Validation
+{
+    /// <summary>
+    /// Defend against invalid states/parameters/arguments when the
+    /// state/parameter/argument is an object. Each of the methods
+    /// defines a "guard". The guard validates that the
+    /// state/parameter/argument does not have a particular type of
+    /// potential error. If the guard finds the
+    /// state/parameter/argument to be in error, an exception is
+    /// thrown. If the value is validated, the value is returned,
+    /// allowing for a fluent guard API.
+    /// </summary>
+    public static class ObjectGuards
+    {
+        /// <summary>
+        /// Guard that the item is not null.
+        /// </summary>
+        /// <param name="item">Item guard is called on.</param>
+        /// <param name="argumentName">
+        /// Name of argument/parameter/state that is being guarded.
+        /// </param>
+        /// <param name="exceptionMessage">
+        /// Exception message if the item is null.
+        /// </param>
+        /// <returns>
+        /// The object is returned to allow a fluent interface with
+        /// other guards.
+        /// </returns>
+        public static object GuardIsNotNull(this object item, string argumentName, string exceptionMessage)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(argumentName, exceptionMessage);
+            }
+
+            return item;
+        }
+
+        /// <summary>
+        /// Guard that a required item is available for an operation.
+        /// This tests that the item is null and if it is, throws an exception.
+        /// </summary>
+        /// <param name="item">Item guard is called on.</param>
+        /// <param name="exceptionMessage">
+        /// Message explaining why the operation is invalid.
+        /// </param>
+        /// <returns>
+        /// The object is returned to allow a fluent interface with
+        /// other guards.
+        /// </returns>
+        public static object GuardIsRequiredForOperation(this object item, string exceptionMessage)
+        {
+            if (item == null)
+            {
+                throw new InvalidOperationException(exceptionMessage);
+            }
+
+            return item;
+        }
+
+        /// <summary>
+        /// Guard that an item is not the default for it's type.
+        /// </summary>
+        /// <param name="item">Item guard is called on.</param>
+        /// <param name="paramName">
+        /// Name provided to help consumer of exception identify the problem.
+        /// </param>
+        /// <returns>
+        /// The object is returned to allow a fluent interface with
+        /// other guards.
+        /// </returns>
+        /// <remarks>
+        /// For reference types, this is essentially the same as the
+        /// GuardIsNotNull() method.
+        /// </remarks>
+        public static T GuardIsNotDefault<T>(this T item, string paramName)
+        {
+            if (typeof(T).IsValueType)
+            {
+                if (item.Equals(default(T)))
+                {
+                    var exMessage = $"The item cannot be the default value for this type ({typeof(T).Name}).";
+                    throw new ArgumentException(exMessage, paramName);
+                }
+            }
+            else
+            {
+                if (item == null)
+                {
+                    throw new ArgumentNullException(paramName, "The item cannot be null.");
+                }
+            }
+
+            return (T)item;
+        }
+    }
+}
