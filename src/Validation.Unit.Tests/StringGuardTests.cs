f7f2ceb543eac501b14cbe4003863127fa6d1495
Greg McGuffey <greg.mcguffey@cognosante.com>
2016-05-04 17:31:07 -0700

    Added string guards and tests.

---
+++ src/Validation.Unit.Tests/StringGuardTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Gpm.Validation;
+
+namespace Gpm.Validation.Unit.Tests
+{
+    [TestFixture]
+    public class StringGuardTests
+    {
+        [Test]
+        public void GuardIsNotNull_WithString_ReturnsString()
+        {
+            // Arrange
+            var originalString = "A valid, non-null, non-empty, not whitespace only string.";
+
+            // Act
+            var returnedString = originalString.GuardIsSet("any");
+
+            // Assert
+            Assert.AreEqual(expected: originalString, actual: returnedString);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void GuardIsSet_WithNotSetItem_ThrowsException(string param)
+        {
+            var paramName = "param";
+            var expectedMessage = "Must be set to a non-null, non-empty, non-whitespace only string.";
+
+            var ex = Assert.Throws<ArgumentException>(() => param.GuardIsSet(paramName));
+
+            StringAssert.Contains(expectedMessage, ex.Message);
+        }
+
+        [Test]
+        public void GuardIsSet_WithValue_ReturnsValue()
+        {
+            var param = "tested string";
+            var paramName = "param";
+
+            var returned = param.GuardIsSet(paramName);
+
+            Assert.AreSame(param, returned);
+        }
+
+        [TestCase("a")]
+        [TestCase("aaa")]
+        public void GuardRequiredLength_WithWrongLength_ThrowsException(string param)
+        {
+            int requiredLength = 2;
+            var paramName = "param";
+            var actualLenth = param.Length;
+            var pluralizer = actualLenth > 1 ? "s" : string.Empty;
+            var exceptionMessage = $"The text must be {requiredLength} character long, but was {actualLenth} character{pluralizer} long.";
+
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => param.GuardRequiredLength(paramName, requiredLength));
+
+            StringAssert.Contains(exceptionMessage, ex.Message);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void GuardRequiredLength_WithNotSetItem_ThrowsException(string param)
+        {
+            var paramName = "param";
+            int requiredLength = 1;
+            var expectedMessage = "Must be set to a non-null, non-empty, non-whitespace only string.";
+
+            var ex = Assert.Throws<ArgumentException>(() => param.GuardRequiredLength(paramName, requiredLength));
+
+            StringAssert.Contains(expectedMessage, ex.Message);
+        }
+
+        [TestCase("a", 1)]
+        [TestCase("aa", 2)]
+        [TestCase("aaa", 3)]
+        public void GuardRequiredLength_WithCorrectLength_ReturnsValue(string param, int requiredLength)
+        {
+            var paramName = "param";
+
+            var returned = param.GuardRequiredLength(paramName, requiredLength);
+
+            Assert.AreSame(param, returned);
+        }
+
+        [Test]
+        public void GuardRequiredLength_WithRequiredZeroLength_ThrowsException()
+        {
+            var param = "anything";
+            var paramName = "param";
+            int requiredLength = 0;
+            var desiredMessage = "The required length must be greater than zero.";
+            var expectedMessage = "The value, 0, is below the minimum limit of 1.";
+
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => param.GuardRequiredLength(paramName, requiredLength));
+
+            StringAssert.Contains(expectedMessage, ex.Message);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void GuardSize_WithNotSetItem_ThrowsException(string param)
+        {
+            var paramName = "param";
+            int min = 2;
+            int max = 3;
+            var expectedMessage = "Must be set to a non-null, non-empty, non-whitespace only string.";
+            //var expectedMessage = string.Format( "The text must be between {0} and {1} characters long.", min, max );
+
+            var ex = Assert.Throws<ArgumentException>(() => param.GuardSize(paramName, min, max));
+
+            StringAssert.Contains(expectedMessage, ex.Message);
+        }
+
+        [TestCase("a")]
+        [TestCase("aaaa")]
+        public void GuardSize_WithWrongSize_ThrowsException(string param)
+        {
+            var paramName = "param";
+            int min = 2;
+            int max = 3;
+            var expectedMessage = string.Format("The text must be between {0} and {1} characters long.", min, max);
+
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => param.GuardSize(paramName, min, max));
+
+            StringAssert.Contains(expectedMessage, ex.Message);
+        }
+
+        [TestCase("aa")]
+        [TestCase("aaa")]
+        public void GuardSize_WithCorrectSize_ReturnsValue(string param)
+        {
+            var paramName = "param";
+            int min = 2;
+            int max = 3;
+
+            var returned = param.GuardSize(paramName, min, max);
+
+            Assert.AreSame(param, returned);
+        }
+    }
+}
---
+++ src/Validation/StringGuards.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gpm.Validation
+{
+    /// <summary>
+    /// Defend against invalid states/parameters/arguments when the
+    /// state/parameter/argument is a string. Each of the methods
+    /// defines a "guard". The guard validates that the
+    /// state/parameter/argument does not have a particular type of
+    /// potential error. If the guard finds the
+    /// state/parameter/argument to be in error, an exception is
+    /// thrown. If the value is validated, the value is returned,
+    /// allowing for a fluent guard API.
+    /// </summary>
+    public static class StringGuards
+    {
+        /// <summary>
+        /// Guard that a string variable has been set. This means that
+        /// is not null, empty or only white space.
+        /// </summary>
+        /// <param name="value">String being tested.</param>
+        /// <param name="argumentName">
+        /// Name of the argument/parameter/state that is being tested.
+        /// </param>
+        /// <returns>The string being tested, to allow fluent interface.</returns>
+        public static string GuardIsSet(this string value, string argumentName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Must be set to a non-null, non-empty, non-whitespace only string.", argumentName);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Guard that a string is of the required length. This assumes
+        /// that the length must be greater than zero and that the
+        /// string must be set.
+        /// </summary>
+        /// <param name="value">String being tested.</param>
+        /// <param name="argumentName">
+        /// Name of the argument/parameter/state that is being tested.
+        /// </param>
+        /// <param name="requiredLength">Required length of text.</param>
+        /// <returns>The string being tested, to allow fluent interface.</returns>
+        public static string GuardRequiredLength(this string value, string argumentName, int requiredLength)
+        {
+            //-- Guard that the value is not null, empty or only white space.
+            value.GuardIsSet(argumentName);
+            requiredLength.GuardMinimum(nameof(requiredLength), 1);
+
+            int actualLength = value.Length;
+            if (actualLength != requiredLength)
+            {
+                var exceptionMessage = $"The text must be {requiredLength} character long, but was {actualLength} character{(actualLength > 1 ? "s" : "")} long.";
+                throw new ArgumentOutOfRangeException(argumentName, exceptionMessage);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Guard that a string is a valid size.
+        /// </summary>
+        /// <param name="value">String being tested.</param>
+        /// <param name="argumentName">
+        /// Name of the argument/parameter/state that is being tested.
+        /// </param>
+        /// <param name="min">Minimum length.</param>
+        /// <param name="max">Maximum length.</param>
+        /// <returns>The string being tested, to allow fluent interface.</returns>
+        public static string GuardSize(this string value, string argumentName, int min, int max)
+        {
+            //-- Guard that the string is set.
+            value.GuardIsSet(argumentName);
+
+            int actualSize = value.Length;
+            var isCorrectSize = min <= actualSize && actualSize <= max;
+            if (!isCorrectSize)
+            {
+                var exceptionMessage = string.Format("The text must be between {0} and {1} characters long.", min, max);
+                throw new ArgumentOutOfRangeException(argumentName, exceptionMessage);
+            }
+
+            return value;
+        }
+    }
+}
