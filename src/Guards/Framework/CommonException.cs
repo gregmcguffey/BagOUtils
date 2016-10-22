75fe4a5f9f652102870527d1274d423b13240906
Greg McGuffey <greg.mcguffey@cognosante.com>
2016-10-22 13:27:50 -0700

    Added static class that creates some common  exceptions.

---
+++ src/Guards/Framework/CommonException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagOUtils.Guards.Framework
+{
+    public static class CommonException
+    {
+        public static Func<string, Func<ArgumentException>> SimpleArgument = message => () => new ArgumentException(message);
+        public static Func<string, string, Func<ArgumentException>> Argument = (item, message) => () => new ArgumentException(message, item);
+
+        public static Func<string, Func<ArgumentNullException>> SimpleArgumentNull = item => () => new ArgumentNullException(item);
+        public static Func<string, string, Func<ArgumentNullException>> ArgumentNull = (item, message) => () => new ArgumentNullException(item, message);
+
+        public static Func<string, Func<ArgumentOutOfRangeException>> SimpleOutOfRange = item => () => new ArgumentOutOfRangeException(item);
+        public static Func<string, string, Func<ArgumentOutOfRangeException>> OutOfRange = (item, message) => () => new ArgumentOutOfRangeException(item, message);
+
+        public static Func<string, Func<InvalidOperationException>> InvalidOperation = message => () => new InvalidOperationException(message);
+    }
+}
