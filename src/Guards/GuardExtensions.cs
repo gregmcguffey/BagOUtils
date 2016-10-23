d267bfa12e9f95b94106f66f9df94b52cf9bacc0
gregm <greg.mcguffey@cognosante.com>
2016-10-23 11:05:13 -0700

    Removed framework concept.
    It was too complicated and added little value. It also likely would have affected performance.

--- src/Guards/GuardExtensions.cs
+++ src/Guards/GuardExtensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BagOUtils.Guards.Messages;
 
-namespace BagOUtils.Guards.Framework
+namespace BagOUtils.Guards
 {
     /// <summary>
     /// Extensions methods that allow fluent comparison of numbers. This
@@ -17,71 +17,6 @@ namespace BagOUtils.Guards.Framework
     /// </summary>
     public static class GuardExtensions
     {
-        //-------------------------------------------------------------------------
-        //
-        // Guard Initiators
-        //
-        //-------------------------------------------------------------------------
-
-        /// <summary>
-        /// Initiate a guard that will use an explicit message
-        /// if the guard fails and an exception is thrown.
-        /// </summary>
-        /// <typeparam name="TValue">type of value being checked.</typeparam>
-        /// <typeparam name="TException">Type of exception to throw.</typeparam>
-        /// <param name="value">Value to check.</param>
-        /// <returns>Guard to fluently configure.</returns>
-        public static MessageTemplateComposer<TValue> ComposeMessageTemplateGuard<TValue>(this TValue value)
-        {
-            var guard = new MessageTemplateComposer<TValue>(value);
-            return guard;
-        }
-
-        /// <summary>
-        /// Initiate a guard that uses a template to inject the name of
-        /// the item being checked.
-        /// </summary>
-        /// <typeparam name="TValue">type of value being checked.</typeparam>
-        /// <typeparam name="TException">Type of exception to throw.</typeparam>
-        /// <param name="value">Value to check.</param>
-        /// <returns>Guard to fluently configure.</returns>
-        public static ItemTemplateComposer<TValue> ComposeItemTemplateGuard<TValue>(this TValue value)
-        {
-            var composer = new ItemTemplateComposer<TValue>(value);
-            return composer;
-        }
-
-        /// <summary>
-        /// Initiate a guard that is checking on minimum or maximum limits
-        /// of a value. A template is used to create a message indicating 
-        /// if the value is too large or small.
-        /// </summary>
-        /// <typeparam name="TValue">type of value being checked.</typeparam>
-        /// <typeparam name="TException">Type of exception to throw.</typeparam>
-        /// <param name="value">Value to check.</param>
-        /// <returns>Guard to fluently configure.</returns>
-        public static LimitGuard<TValue, TException> PrepareLimitGuard<TValue, TException>(this TValue value)
-            where TException : Exception
-            where TValue : IComparable<TValue>
-        {
-            var guard = new LimitGuard<TValue, TException>(value);
-            return guard;
-        }
-
-        /// <summary>
-        /// Initiate a guard that uses an explicitly provided message for
-        /// the exception that is thrown if the value does not pass the guard.
-        /// </summary>
-        /// <typeparam name="TValue">type of value being checked.</typeparam>
-        /// <typeparam name="TException">Type of exception to throw.</typeparam>
-        /// <param name="value">Value to check.</param>
-        /// <returns>Guard to fluently configure.</returns>
-        public static MessageComposer<TValue> PrepareMessageGuard<TValue>(this TValue value)
-        {
-            var composer = new MessageComposer<TValue>(value);
-            return composer;
-        }
-
 
         //-------------------------------------------------------------------------
         //
